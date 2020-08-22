c119c637b29bbb08b476585ebf3ba2ec077488cb
Kritika Srivastava <kritika.s1408@gmail.com>
2020-08-22 14:43:12 +0530

    Created UniverseState.java

---
+++ UniverseState.java
@@ -0,0 +1,12 @@
+class UniverseState {
+    public void Universe(String[][] a, int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
